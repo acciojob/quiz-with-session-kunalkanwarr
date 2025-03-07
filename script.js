031932edddc87c2dd1e63f69f977cad2282cb0e3
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-07 19:02:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const questions = [
-        { question: "What is 2 + 2?", options: ["3", "4", "5", "6"], answer: "4" },
         { question: "What is the capital of France?", options: ["Berlin", "Madrid", "Paris", "Lisbon"], answer: "Paris" },
+		{ question: "What is 2 + 2?", options: ["3", "4", "5", "6"], answer: "4" },
         { question: "Which planet is known as the Red Planet?", options: ["Earth", "Mars", "Jupiter", "Venus"], answer: "Mars" },
         { question: "What is the largest mammal?", options: ["Elephant", "Blue Whale", "Giraffe", "Hippopotamus"], answer: "Blue Whale" },
         { question: "How many continents are there?", options: ["5", "6", "7", "8"], answer: "7" }

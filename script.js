b8d6e2abaf599b4fbd1ba3f432519201e19387a2
Kunal Kanwar <113274734+kunalkanwarr@users.noreply.github.com>
2025-03-07 19:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const questions = [
         { question: "What is the capital of France?", options: ["Paris", "London", "Berlin", "Madrid"], answer: "Paris" },
-		{ question: "What is the highest mountain in the world?", options: ["Everest", "Kilimanjaro", "", ""], answer: "Mount Everest" },
+		{ question: "What is the highest mountain in the world?", options: ["Everest", "Kilimanjaro", "Denali", ""], answer: "Mount Everest" },
         { question: "What is the largest country by area?", options: ["Cuba", "India", "Japan", "Russia"], answer: "Russia" },
         { question: "Which is the largest planet in our solar system?", options: ["Saturn", "Earth", "Neptune", "Jupiter"], answer: "Jupiter" },
         { question: "What is the capital of Canada?", options: ["Toronto", "Montreal", "Ottawa", "Vancouver"], answer: "Ottawa" }
